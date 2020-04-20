ffa73b91fe0b455d3dad30688c9d83f71374684d
Natalia Banasiewicz <natalia.banasiewicz@wp.pl>
2020-04-20 17:43:58 +0200

    Add middleware for paths

--- server.js
+++ server.js
@@ -3,25 +3,37 @@ const path = require('path');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname + `/views/${name}`));
+  };
+  next();
+});
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/index.html'));
+  res.show('index.html');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/about.html'));
+  res.show('about.html');
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/contact.html'));
+  res.show('contact.html');
 });
 
 app.get('/info', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/info.html'));
+  res.show('info.html');
 });
 
 app.get('/history', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/history.html'));
+  res.show('history.html');
 });
+
+// catch incorrect requests
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+})
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
